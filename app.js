d6d8bea89b1009dd745a777b512871af2303b8d5
dargaCode <darga.code@gmail.com>
2016-06-24 05:34:54 -0700

    Add boilerplate express code
    - Add dependencies
    - Add settings
    - Add root route
    - Add basic server listening
    - Add database connection

---
+++ app.js
@@ -0,0 +1,69 @@
+
+'use strict'; //enable 'let'
+
+// DEPENDENCIES - NPM PACKAGES
+
+var express = require('express');
+var app = express();
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// DEPENDENCIES - DATABASE MODELS
+
+
+
+// DEPENDENCIES - OTHER LOCAL SCRIPTS
+
+const pjson = require('./package.json');
+
+// CONSTANTS
+
+const APP_NAME = pjson.name;
+const DEFAULT_PORT = 3000;
+const PORT = process.env.PORT || DEFAULT_PORT;
+const SERVER_MESSAGE = `Serving ${APP_NAME} on port ${PORT}`;
+const DEFAULT_DATABASE_URL = `mongodb://localhost/${APP_NAME}`;
+const DATABASE_URL = process.env.DATABASE_URL || DEFAULT_DATABASE_URL;
+const DATABASE_MESSAGE = `Connected to database at ${DATABASE_URL}`;
+
+// SETTINGS
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+ejs.delimiter = '?';
+
+// VARIABLES
+
+
+
+// ROUTES
+
+  // ROUTE - ROOT
+app.get('/', function(req,res) {
+  res.send(APP_NAME + ' root route');
+});
+
+
+// FUNCTIONS
+
+function initialize() {
+  serverListen();
+  databaseConnect();
+};
+
+function serverListen() {
+  app.listen(PORT, function() {
+    console.log(SERVER_MESSAGE);
+  });
+}
+
+function databaseConnect() {
+  mongoose.connect(DATABASE_URL, function() {
+    console.log(DATABASE_MESSAGE);
+  });
+};
+
+// MAIN
+
+initialize();
